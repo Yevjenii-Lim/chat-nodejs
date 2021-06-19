eec30c3310fa68fdffff18681ac6f4a7d3254b86
Yevjenii-Lim <pacmanlim27@gmail.com>
2021-06-19 16:46:37 +0300

    add some fetures

--- src/components/OneRoom.jsx
+++ src/components/OneRoom.jsx
@@ -1,19 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Redirect, useRouteMatch } from "react-router";
 import "./messags.css";
 import socket from "./socket";
-
+import "./oneroom.css";
 function OneRoom({ username }) {
   const router = useRouteMatch();
   let [messags, setMessages] = useState([]);
   let [message, setMessage] = useState("");
-//   let [triger, setTriger] = useState(true);
+  let [file, setFile] = useState()
+  //   let [triger, setTriger] = useState(true);
   let room = useRouteMatch().params.room;
 
-
   useEffect(() => {
-   
-    socket.emit("connection")
+    socket.emit("connection");
     async function getMessages() {
       let result = await fetch("http://localhost:3000/api/rooms/" + room, {
         method: "GET",
@@ -32,48 +31,54 @@ function OneRoom({ username }) {
   }, [room]);
 
   useEffect(() => {
-      
     // socket.emit("chatmessage", { author: username, msg: message, room: room }, ({status}) => {
     //     console.log(status)
     // });
-    socket.emit("chatmessage", { author: username, msg: "test", room: room })
-    console.log("userefec")
+    console.log(messags)
+    socket.emit("chatmessage", { author: username, msg: "test", room: room });
+    console.log("userefec");
     return () => {
-        socket.emit("disconect")
-        socket.off()
-    }
+      socket.emit("disconect");
+      socket.off();
+    };
   }, []);
 
   useEffect(() => {
-   
     socket.on("message", (message) => {
-        console.log(message)
-        // console.log(message)
-        if(message.room != room) {
-            console.log('wrong room')
-        }else {
+      console.log(message);
+      // console.log(message)
+      if (message.room != room) {
+        console.log("wrong room");
+      } else {
+        //   let blob = new Blob([message])
+        //   let url = URL.createObjectURL(blob)
+          
+        setMessages([...messags, message]);
+      }
+      // setMessages(message.text)
+    });
+  }, [messags]);
 
-            setMessages([...messags, message])
-        }
-        // setMessages(message.text)
-    })
-  }, [messags])
 
 
   async function sendMessage(event) {
     event.preventDefault();
+ 
 
-    if(message) {
-        console.log("send")
-        socket.emit("chatmessage", { author: username, msg: message, room: room }, (error) => {
-            if(error) {
-                console.log(error)
-
-            }
-        }); 
-        setMessage("")
+    if (message) {
+      console.log("send");
+      socket.emit(
+        "chatmessage",
+        { author: username, msg: message, room: room },
+        (error) => {
+          if (error) {
+            console.log(error);
+          }
+        }
+      );
+      setMessage("");
     }
-
+    
     // if(username.length === 0) {
     //     console.log("no user")
     //     return
@@ -96,9 +101,40 @@ function OneRoom({ username }) {
     //   alert(result.message);
     // }
   }
+  const handleFileSelected = (e) => {
+    const files = e.target.files
+    console.log("files:", files)
+  }
+  let refContainer = useRef(null)
+  let sendFile = async (event) => {
+    event.preventDefault();
+    
+    console.log(refContainer.current.files[0])
+    let photo = new FormData();
+     photo.append("author", username)
+     photo.append("room", room)
+    photo.append("photo", refContainer.current.files[0]);
+    
+    let result = await fetch("http://localhost:3000/api/upload", {
+        method: "POST",
+        
+        body: photo
+      
+      }).then((res) => res.json());
+      if (result.status === "ok") {
+     
+        alert(result.message);
+
+      } else {
+        alert(result.message);
+      }
+     
+ 
+  }
   if (!!username === false) {
     return <Redirect to="/" />;
   }
+
   return (
     <div>
       <div className="block-panel">
@@ -124,17 +160,45 @@ function OneRoom({ username }) {
                 autoComplete="off"
               ></input>
             </div>
+            <div className="input-group">
+              <span className="input-group-btn">
+                <button
+                  className="btn btn-default"
+                  type="button"
+                  onClick={sendFile}
+                >
+                  Send file
+                </button>
+              </span>
+              <input
+                type="file"
+                id="text"
+                className="form-control"
+                // value={file}
+                // onChange={(e) => setFile(e.target.value)}
+                onChange={handleFileSelected}
+                autoComplete="off"
+                ref={refContainer}
+              ></input>
+            </div>
           </div>
         </div>
         <div className="panel panel-default">
           <div className="panel-body chat-body">
-            {messags.length > 0 ? (
-              messags.map((i, index) => (
-                <Message key={index} author={i.author} text={i.text}></Message>
-              ))
-            ) : (
-              <p>No messages</p>
-            )}
+            
+              {messags.length > 0 ? (
+                messags.map((i, index) => (
+                  <Message
+                    key={index}
+                    author={i.author}
+                    text={i.text}
+                    photo={i.photo}
+                  ></Message>
+                ))
+              ) : (
+                <p>No messages</p>
+              )}
+        
 
             {/* <span className="message"><span className="user" style="color: #0A6">User#1</span>: Hello.</span> */}
           </div>
@@ -144,12 +208,13 @@ function OneRoom({ username }) {
   );
 }
 
-let Message = ({ author, text }) => {
+let Message = ({ author, text, photo }) => {
   return (
     <div className="message">
-      <p>Author: {author}</p>
-      <p>Message: {text}</p>
-    
+      <span>{author}:  </span>
+      <span>{text}</span>
+      
+      {photo ? <img src={"file:///"+photo}></img> : null}
     </div>
   );
 };
